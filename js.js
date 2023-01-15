8a614c7dfa287b24facfe7e0603adc7484e7dc04
MorurB <118909455+MorurB@users.noreply.github.com>
2023-01-15 13:08:48 +0200

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.menu');
-let header__list = document.querySelector('.header__list');
-menuBtn.addEventListener('click', function () {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
-    header__list.classList.toggle('header__list_active');
-})
